3cfa67b7925c9db5c3f658ee4b41ce4a1cd8e78f
yana32417@gemail.com <yana32417@gemail.com>
2023-10-09 11:01:40 +0700

    [WIP] menambahkan konfigurasi firebase

---
+++ konfigurasi.firebase.js
@@ -0,0 +1,9 @@
+export  const konfigurasifirebase = {
+    apiKey: "AIzaSyAZe_k0qbCwRK8DlwSmskyTxlTks73Wc94",
+    authDomain: "contoh-perojek.firebaseapp.com",
+    projectId: "contoh-perojek",
+    storageBucket: "contoh-perojek.appspot.com",
+    messagingSenderId: "501042387828",
+    appId: "1:501042387828:web:1a4b4a0ad9547aff7aea21",
+    measurementId: "G-X8PZ8TMF02"
+  };
